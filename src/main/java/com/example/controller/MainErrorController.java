7f504ac9fe3702fdc7ab819b2ae9a9cd65d95511
赵西宝 <zxb_5211@163.com>
2017-11-04 10:01:52 +0800

    11

--- src/main/java/com/example/controller/MainErrorController.java
+++ src/main/java/com/example/controller/MainErrorController.java
@@ -1,29 +1,25 @@
 package com.example.controller;
 
-import java.io.PrintStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class MainErrorController
-  implements ErrorController
-{
-  public String getErrorPath()
-  {
-    return null;
-  }
+public class MainErrorController implements ErrorController {
+	public String getErrorPath() {
+		return null;
+	}
 
-  public String handlerError(HttpServletRequest req, HttpServletResponse resp)
-  {
-    System.out.println(resp.getStatus() + "          !");
-    if (resp.getStatus() == 404) {
-      return "forward:404.html";
-    }
-    if (resp.getStatus() == 500) {
-      return "forward:500.html";
-    }
-    return "forward:index.html";
-  }
+	public String handlerError(HttpServletRequest req, HttpServletResponse resp) {
+		System.out.println(resp.getStatus() + "          !");
+		if (resp.getStatus() == 404) {
+			return "forward:404.html";
+		}
+		if (resp.getStatus() == 500) {
+			return "forward:500.html";
+		}
+		return "forward:index.html";
+	}
 }
