f5bbfe2ebe52c6f1ad70f7c38dbb18c293d2f189
赵西宝 <zxb_5211@163.com>
2017-11-04 09:39:52 +0800

    test

--- src/test/java/com/example/PhoneApplicationTests.java
+++ src/test/java/com/example/PhoneApplicationTests.java
@@ -2,17 +2,27 @@ package com.example;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.example.domain.Girl;
+import com.example.domain.GridPageBean;
+import com.example.service.GirlService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PhoneApplicationTests {
-
+	
+	@Autowired
+	private GirlService girlService;
+	
 	@Test
 	public void contextLoads() {
-		System.out.println("1");
+		String cupSize = "";
+		int pageNo = 1;
+		GridPageBean<Girl> result = girlService.findAll(cupSize, pageNo);
+		System.out.println(result.getTotal());
 	}
 
 }
