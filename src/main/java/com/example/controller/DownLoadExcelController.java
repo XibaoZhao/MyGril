f7ec7b7efc073a066a7d04b90cc366564a5a18e3
赵西宝 <zxb_5211@163.com>
2017-11-07 10:37:19 +0800

    乱码

--- src/main/java/com/example/controller/DownLoadExcelController.java
+++ src/main/java/com/example/controller/DownLoadExcelController.java
@@ -25,10 +25,11 @@ public class DownLoadExcelController {
 	@RequestMapping(value="/excel",method=RequestMethod.GET)
 	public void downLoad(HttpServletResponse rep) throws Exception{
 		downService.down(filePath);
-		String fileName = "excel.xlsx";
+		String fileName = "表格.xlsx";
+		String file = new String(fileName.getBytes("gb2312"),"ISO8859-1");
 		rep.setContentType("application/vnd.ms-excel;charset=utf-8");
 		rep.setCharacterEncoding("utf-8");
-		rep.setHeader("Content-Disposition", "attachment;filename=" + fileName);
+		rep.setHeader("Content-Disposition", "attachment;filename=" + file);
 		byte[] buff = new byte[1024];
 		FileInputStream fis = new FileInputStream(filePath);
 		ServletOutputStream ops = rep.getOutputStream();
